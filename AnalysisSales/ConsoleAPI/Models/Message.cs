ab0e96beb5f4799e9ff5e391a5d325c60f786b06
@paglebov_1 <paglebov_1@edu.hse.ru>
2024-03-06 20:31:42 +0300

    GeneralCommit

---
+++ AnalysisSales/ConsoleAPI/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace AnalysisSales.Models;
+
+public class Message(string messageLine)
+{
+    public string MessageLine { get; set; } = messageLine;
+    
+    public Message() : this("") { }
+
+    public override string ToString() => MessageLine;
+}
---
+++ AnalysisSales/Domain/Interfaces/ISeasonalityProductsRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Interfaces;
+
+public interface ISeasonalityProductsRepository
+{
+    double GetCoefficient(long idProduct, int month);
+}
